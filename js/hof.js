bb8ad36dcc2f1c3b380fa15714cd7dbafce17bfd
PC <kim000183@naver.com>
2022-02-10 23:15:34 +0900

    3이상 나오는 함수 진행중..

--- js/hof.js
+++ js/hof.js
@@ -1,4 +1,6 @@
 // 고차함수 예제풀이
+
+// 3씩 곱해지는 함수를 작성
 function data(a){
     return a * 3
 } 
@@ -14,3 +16,25 @@ function solution(arr, fn){
 
 console.log(solution([1,2,3,4,5], data))
 
+
+// 배열이 3 이상만 나오는 함수를 작성
+function data(a){
+    if(a >= 3){
+        return a;
+    }
+} 
+
+
+function solution(arr, fn){
+    let num = arr;
+    let char = [];
+    for(let i  of num){
+        if(fn(i) != undefined){
+            char.push(i);
+        }
+    }
+    return char;
+}
+
+
+console.log(solution([1,2,3,4,5], data))
