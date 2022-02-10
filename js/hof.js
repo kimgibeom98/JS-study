a8f3825515b29d6a43f85c1e32830d4c577f2ccc
PC <kim000183@naver.com>
2022-02-10 22:47:59 +0900

    고차함수 예제 1번 완료

--- js/hof.js
+++ js/hof.js
@@ -1,20 +1,16 @@
 // 고차함수 예제풀이
-
-function data(i){
-    let result = i * 3;
-    return result;
+function data(a){
+    return a * 3
 } 
-
-
 function solution(arr, fn){
     let num = arr;
     let char = [];
     for(let i  of num){
-        char.push(fn(i));
+            char.push(fn(i));
     }
     return char;
 }
 
 
-console.log(solution([1,2,3,4,5], data()))
+console.log(solution([1,2,3,4,5], data))
 
