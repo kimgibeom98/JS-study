b46365f2ca228f3e052e3409077f72780edc2b57
PC <kim000183@naver.com>
2022-02-14 21:34:54 +0900

    고차함수 예제 완료

--- js/hof.js
+++ js/hof.js
@@ -19,17 +19,15 @@ console.log(solution([1,2,3,4,5], data))
 
 // 배열이 3 이상만 나오는 함수를 작성 data함수에서 return 값을 boolean으로 받기
 function data(a){
-    if(a >= 3){
-        return a;
-    }
+    return a >= 3;
 } 
 
 
 function solution(arr, fn){
     let num = arr;
-    let char = [];
+    const char = [];
     for(let i  of num){
-        if(fn(i) != undefined){
+        if(fn(i)){
             char.push(i);
         }
     }
@@ -37,4 +35,5 @@ function solution(arr, fn){
 }
 
 
-console.log(solution([1,2,3,4,5], data))
+document.write(solution([1,2,3,4,5], data))
+
