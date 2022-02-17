92891505074563e16c9a2f3681e4fddab923af79
PC <kim000183@naver.com>
2022-02-17 22:47:46 +0900

    문제수정

--- js/hof.js
+++ js/hof.js
@@ -90,4 +90,5 @@ function solution(arr, fn, char){
 
 document.write(solution([1,2,3,4,5], mapeven, filtereven));
 
-// exp.5  배열중 없는 0~9까지 없는 숫자를 찾아서 더하는 함수를 만들어라.
+// exp.5  0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어진다. 
+// numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해라.
