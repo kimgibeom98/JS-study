46a7d08d068a13ca154541cf7b020da50f0f5019
PC <kim000183@naver.com>
2022-02-17 21:18:03 +0900

    map과 filter를 합친 예제풀이

--- js/hof.js
+++ js/hof.js
@@ -1,6 +1,6 @@
 // 고차함수 예제풀이
 
-// 3씩 곱해지는 함수를 작성
+//exp.1 3씩 곱해지는 함수를 작성
 function data(a){
     return a * 3
 } 
@@ -17,7 +17,7 @@ function solution(arr, fn){
 console.log(solution([1,2,3,4,5], data))
 
 
-// 배열이 3 이상만 나오는 함수를 작성 data함수에서 return 값을 boolean으로 받기
+//exp.2 배열이 3 이상만 나오는 함수를 작성 data함수에서 return 값을 boolean으로 받기
 function data(a){
     return a >= 3;
 } 
@@ -37,11 +37,11 @@ function solution(arr, fn){
 document.write(solution([1,2,3,4,5], data))
 
 
-// 위에 진행했던 배열 문제를 filter함수와 map함수를 이용해서 코드를 간소화하기
+// exp.3위에 진행했던 배열 문제를 filter함수와 map함수를 이용해서 코드를 간소화하기
 
 // 1. map
 function solution2(arr){
-    const char = arr.map(a => a * 3);
+    const char = arr.map( a => a * 3);
     return char;
 }
 
@@ -64,3 +64,12 @@ document.write(solution2([1,2,3,4,5]))
 // filter는 조건문을 만족하는 요소들을 반환하는 함수 - 논리연산자 true값을 이용해 배열을 만듬
 
 // map 이 반환하는 새 배열의 요소 개수는 변하지 않지만 filter 가 반환하는 새 배열의 요소 개수는 변한다. filter 는 조건에 따라 요소들이 필터링되어 나오기 때문이다.
+
+
+// map은 산술된 값만을 리턴해서 배열을 추가.
+// filter는 논리연산자의 true값만을 이용해서 배열을 추가
+
+
+// exp.4 solution([1,2,3,4,5], 함수1, 함수2)
+// 함수1에는 map을 이용한 연산함수
+// 함수2에는 filter를 이용한 조건함수를 넣어라
