40a82a93496cddbbb3335291fe266688cffeccab
PC <kim000183@naver.com>
2022-02-18 21:31:57 +0900

    reduce함수를 사용한 배열문제

--- js/hof.js
+++ js/hof.js
@@ -92,3 +92,23 @@ document.write(solution([1,2,3,4,5], mapeven, filtereven));
 
 // exp.5  0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어진다. 
 // numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해라.
+// for문 사용
+function solution(arr){
+    let arrsum = 0;
+    for(let i = 0; i < arr.length; i++){
+        arrsum += arr[i];
+    }
+    return Math.abs(arrsum - 45);
+}
+
+console.log(solution([1,2,3,4,5,6,8]));
+
+
+// reduce함수 사용
+function solution(arr){
+   const total = 0;
+   const datasum = 45 - arr.reduce((acc, dcr) => acc + dcr, total)
+   return datasum;
+}
+
+console.log(solution([1,2,3,4,5,6,8]));
