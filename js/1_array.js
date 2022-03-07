eb3acd6954da8da3ed825e05179b78946fea6b57
PC <kim000183@naver.com>
2022-03-07 23:03:51 +0900

    arguments 간단 설명과 예제

--- js/1_array.js
+++ js/1_array.js
@@ -188,3 +188,16 @@ function sorteven(){
     // - sort라는 오리지널 함수의 동작방법을 값을 전달하는것을 통해서 완전히 바꿀수 있는걸 콜백 이라 한다.
 }
 sorteven();
+
+// arguments
+// 함수에는 arguments라는 변수에 담긴 숨겨진 유사 배열이 있다. 이 배열에는 함수를 호출할 때 입력한 인자가 담겨있다. 아래 예제를 보자. 결과는 10이다.
+function sum(){
+    var i, _sum = 0;    
+    for(i = 0; i < arguments.length; i++){
+        document.write(i+' : '+arguments[i]+'<br />');
+        _sum += arguments[i];
+    }   
+    return _sum;
+}
+document.write('result : ' + sum(1,2,3,4));
+sum();
