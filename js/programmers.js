ab11f96fdb620459c57aeb1efdb7f426b3dc4c6b
PC <kim000183@naver.com>
2022-03-15 21:33:21 +0900

    프로그래머스 문제 추가

--- js/programmers.js
+++ js/programmers.js
@@ -117,3 +117,16 @@ function solution07(num) {
     return (num % 2 === 0) ? "Even" : "Odd";
 }
 solution07(3);
+
+
+// 함수 solution08은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+// 제한 조건
+// n은 1이상 8000000000 이하인 자연수입니다.
+function solution08(n){
+    var answer = (n + '').split('');
+    console.log(answer)
+    answer.sort(function(a,b) {return b - a})
+    return Number(answer.join(''));
+}
+solution08(118372);
