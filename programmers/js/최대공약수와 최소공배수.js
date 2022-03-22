b9960ef116cefdee9d43ff47db861dd084b96f7e
PC <kim000183@naver.com>
2022-03-22 20:32:53 +0900

    프로그래머스 문제추가

---
+++ programmers/js/최대공약수와 최소공배수.js
@@ -0,0 +1,25 @@
+// 문제 설명
+// 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요. 배열의 맨 앞에 최대공약수, 그다음 최소공배수를 넣어 반환하면 됩니다. 예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 반환해야 합니다.
+
+// 제한 사항
+// 두 수는 1이상 1000000이하의 자연수입니다.
+function solution(n, m) {
+    var num1 = [];
+    let total = [];
+    for(let i = 1; i <= Math.min(n,m); i++){
+        if(n % i == 0 && m % i == 0){
+            num1.push(i);
+        }
+    }
+    total.push(Math.max(...num1));
+    let num2 = 1;
+    while(true){
+        if(num2 % n == 0 && num2 % m == 0){
+            break;
+        }
+        num2 ++;
+    }
+    total.push(num2);
+    return total;
+}
+solution(3,12);
