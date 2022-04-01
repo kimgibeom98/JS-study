eec648f0b0e738fd5128b3ebd7cf9f55f0ec5a55
PC <kim000183@naver.com>
2022-04-01 21:12:55 +0900

    프로그래머스 문제추가

---
+++ programmers/js/두 개 뽑아서 더하기.js
@@ -0,0 +1,16 @@
+// 문제 설명
+// 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// numbers의 길이는 2 이상 100 이하입니다.
+// numbers의 모든 수는 0 이상 100 이하입니다.
+function solution(numbers) {
+    let answer = [];
+    for(let i = 0; i < numbers.length; i++){
+        for(j = i + 1; j < numbers.length; j++){
+            answer.push(numbers[i] + numbers[j]);
+        }
+    }
+    return answer.filter((item, index) => answer.indexOf(item) === index).sort((a,b) => a - b);
+}
+solution([2,1,3,4,1]);
