dbca6919487d92893d7deb47a7a59edfe40c5807
PC <kim000183@naver.com>
2022-04-06 23:45:00 +0900

    map

---
+++ 함수형프로그래밍/map,filter,reduce/JS/1.js
@@ -0,0 +1,33 @@
+const products = [
+    {name: '반팔티', price: 15000},
+    {name: '긴팔티', price: 20000},
+    {name: '핸드폰케이스', price: 15000},
+    {name: '후드티', price: 30000},
+    {name: '바지', price: 25000}
+  ];
+
+//   # map
+
+const map = (f, iter) => {
+    let res = [];
+    for (const a of iter) {
+      res.push(f(a));
+    }
+    return res;
+  };
+
+  // let names = [];
+  // for (const p of products) {
+  //   names.push(p.name);
+  // }
+  // log(names);
+
+  console.log(map(p => p.name, products));
+
+  // let prices = [];
+  // for (const p of products) {
+  //   prices.push(p.price);
+  // }
+  // log(prices);
+
+  console.(map(p => p.price, products));
