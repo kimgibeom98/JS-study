096d12125d4be935590c5d5624ca3ef35a53a935
PC <kim000183@naver.com>
2022-04-09 12:07:23 +0900

    reduce

--- 함수형프로그래밍/map,filter,reduce/JS/1.js
+++ 함수형프로그래밍/map,filter,reduce/JS/1.js
@@ -94,3 +94,36 @@ const map = (f, iter) => {
     yield 4;
     yield 5;
   }()));
+
+
+  // # reduce
+
+  const nums = [1, 2, 3, 4, 5];
+
+  let total = 0;
+  for (const n of nums) {
+    total = total + n;
+  }
+  log(total);
+
+  const reduce = (f, acc, iter) => {
+    if (!iter) {
+      iter = acc[Symbol.iterator]();
+      acc = iter.next().value;
+    }
+    for (const a of iter) {
+      acc = f(acc, a);
+    }
+    return acc;
+  };
+
+  const add = (a, b) => a + b;
+
+  console.log(reduce(add, 0, [1, 2, 3, 4, 5]));
+  // 15
+
+  console.log(add(add(add(add(add(0, 1), 2), 3), 4), 5));
+  // 15
+
+  console.log(reduce(add, [1, 2, 3, 4, 5]));
+  // 15
