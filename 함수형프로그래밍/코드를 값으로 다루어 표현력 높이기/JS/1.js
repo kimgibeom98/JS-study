141c852d6996d4a7a947af594fc255d96dc34c74
PC <kim000183@naver.com>
2022-04-12 22:27:44 +0900

    코드를 값으로 다루기 go함수

---
+++ 함수형프로그래밍/코드를 값으로 다루어 표현력 높이기/JS/1.js
@@ -0,0 +1,26 @@
+const products = [
+    {name: '반팔티', price: 15000},
+    {name: '긴팔티', price: 20000},
+    {name: '핸드폰케이스', price: 15000},
+    {name: '후드티', price: 30000},
+    {name: '바지', price: 25000}
+  ];
+
+  const add = (a, b) => a + b;
+
+  console.log(
+    reduce(
+      add,
+      map(p => p.price,
+        filter(p => p.price < 20000, products))));
+
+    // ## go, pipe
+
+    const go = (...args) => reduce((a, f) => f(a), args);
+  const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
+
+  go(
+    add(0, 1),
+    a => a + 10,
+    a => a + 100,);
+  // 111
