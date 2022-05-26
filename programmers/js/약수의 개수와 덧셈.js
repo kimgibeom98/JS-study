965924e48de176d620ad47b89c3e2e487a60d856
PC <kim000183@naver.com>
2022-05-26 22:04:27 +0900

    약수의 개수와 덧셈 완료

--- programmers/js/약수의 개수와 덧셈.js
+++ programmers/js/약수의 개수와 덧셈.js
@@ -1,15 +1,28 @@
-function solution(left, right) {
-    let answer = 0;
-    for(let i = left; i <= right; i++){
-        for(let j = 0; j <= left; j++){
-            if(left % j === 0){
-                answer = i;
-            } 
-            if(answer.length % 2 === 0){
-                
-            }
+
+function divisors(num) {
+    let count = 0;
+    console.log(num)
+
+    for(let i = 1; i <= num; i++) {
+        if(num % i == 0) {
+            count++;
         }
     }
-    console.log(answer)
-    // return answer;
+
+    return count;
+}
+
+function solution(left, right) {
+    let answer = 0;
+
+    for(let i = left; i <= right; i++) {
+        let count = divisors(i);          
+        if (count % 2 === 0) {
+            answer += i;
+        } else {
+            answer -= i;
+        }      
+    }   
+
+    return answer;
 }
