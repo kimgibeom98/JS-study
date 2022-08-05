f37bc3e5436eec3c29d075106a4fe9d643704386
PC <kim000183@naver.com>
2022-08-05 16:27:02 +0900

    스크립트 수정중

--- JS/js/stickynotespractice.js
+++ JS/js/stickynotespractice.js
@@ -1,37 +1,35 @@
 
 window.onload = function(){
+
     const $boxwidth = document.querySelector('#ip-width');
     const $boxheight = document.querySelector('#ip-height');
     const $boxtop = document.querySelector('#ip-top');
     const $boxleft = document.querySelector('#ip-left');
     const $insertbtn = document.querySelector('#insert-btn');
-    const $stickynote = document.querySelector('#sticky-note');
+    const $body = document.querySelector('#body');
+    let count = 1;
 
-    $insertbtn.addEventListener('click', () => {
-        setTimeout(() => {
-            $stickynote.style.width = `${$boxwidth.value}px`;
-            $stickynote.style.height = `${$boxheight.value}px`;
-            $stickynote.style.top = `${$boxtop.value}%`;
-            $stickynote.style.left = `${$boxleft.value}%`;
-        }, 3000); 
+        $insertbtn.addEventListener('click', () => {
+            setTimeout(() => {  
 
-    });
+                let newDiv = document.createElement("div");
+                newDiv.classList.add(`note-box${count}`);
 
-}
+                let newTextArea = document.createElement("textarea");
+                newTextArea.placeholder = '메모를 입력하세요...';
+                newDiv.appendChild(newTextArea);
 
-window.onload = function(){
-    const $insertbtn = document.querySelector('#insert-btn');
-    const $stickynote = document.querySelector('#sticky-note');
+                $body.appendChild(newDiv);
 
-    $insertbtn.addEventListener('click', () => {
-        setTimeout(() => {
-            $stickynote.style.width = `${document.querySelector('#ip-width').value}px`;
-            $stickynote.style.height = `${document.querySelector('#ip-height').value}px`;
-            $stickynote.style.top = `${document.querySelector('#ip-top').value}%`;
-            $stickynote.style.left = `${document.querySelector('#ip-left').value}%`;
-        }, 3000); 
+                const $stickynote = document.querySelector(`.note-box${count} > textarea`);
 
-    });
-
+                $stickynote.style.width = `${$boxwidth.value}px`;
+                $stickynote.style.height = `${$boxheight.value}px`;
+                $stickynote.style.top = `${$boxtop.value}%`;
+                $stickynote.style.left = `${$boxleft.value}%`;
+                
+                count ++;
+            }, 1000);
+        });
 }
 
