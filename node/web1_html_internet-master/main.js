d8c2fe28d9c84106d62260a0c357be6f54a860e5
PC <kim000183@naver.com>
2022-10-17 20:41:45 +0900

    생활코딩 웹서버 만들기

---
+++ node/web1_html_internet-master/main.js
@@ -0,0 +1,15 @@
+let http = require('http');
+let fs = require('fs');
+let app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
+app.listen(3000);
