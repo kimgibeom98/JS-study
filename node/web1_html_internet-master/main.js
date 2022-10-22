3696f952be0bf9d4a115b4b4beb7e590226f3283
PC <kim000183@naver.com>
2022-10-22 14:36:18 +0900

    함수를 이용해서 정리

--- node/web1_html_internet-master/main.js
+++ node/web1_html_internet-master/main.js
@@ -2,6 +2,21 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
+function templateHTML(title, list, body){
+  return ` <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>`;
+}
+
 let app = http.createServer(function (request, response) {
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
@@ -17,22 +32,7 @@ let app = http.createServer(function (request, response) {
           list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
         }
         list = list + '</ul>'
-        const template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>
-          ${description}
-          </p>
-        </body>
-        </html>`;
+        const template = templateHTML(title, list,`<h2>${title}</h2>${description}`)
         response.writeHead(200);
         response.end(template);
       });
@@ -44,22 +44,7 @@ let app = http.createServer(function (request, response) {
         }
         list = list + '</ul>'
         fs.readFile(`data/${title}`, 'utf8', function (err, description) {
-          const template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>
-            ${description}
-            </p>
-          </body>
-          </html>`;
+          const template = templateHTML(title, list,`<h2>${title}</h2>${description}`)
           response.writeHead(200);
           response.end(template);
         });
@@ -69,7 +54,5 @@ let app = http.createServer(function (request, response) {
     response.writeHead(404);
     response.end("Not found");
   }
-
-
 });
 app.listen(3000);
