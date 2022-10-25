f6a10e5f4f7804e7673c0191537ccfa0327613cb
PC <kim000183@naver.com>
2022-10-25 22:08:44 +0900

    글생성 UI만들기

--- node/web1_html_internet-master/main.js
+++ node/web1_html_internet-master/main.js
@@ -12,6 +12,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>`;
@@ -51,6 +52,36 @@ let app = http.createServer((request, response) => {
         });
       });
     }
+  } else if (pathname === '/create') {
+    fs.readdir('./data', (err, filelist) => {
+      title = 'WEB - create';
+      const list = templateList(filelist);
+      const template = templateHTML(title, list,`
+      <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <form action="http://localhost:3000/process_create" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description" id="" cols="30" rows="10" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+            
+        </body>
+        </html>
+      `)
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     response.writeHead(404);
     response.end("Not found");
