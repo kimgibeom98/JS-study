1f7ea1ea907d39c7993b920a614e872d20b8f94a
PC <kim000183@naver.com>
2022-11-03 21:16:37 +0900

    Node.js 모듈의형식

---
+++ node/web1_html_internet-master/nodejs/muse.js
@@ -0,0 +1,12 @@
+// const  M = {
+//     v : 'v',
+//     f : function() {
+//         console.log(this.v)
+//     }
+// }
+
+const part = require('./mpart.js');
+// console.log(part)
+
+
+part.f();
