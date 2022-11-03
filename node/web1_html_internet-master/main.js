fc662ce6f53945d4412e63172faee2ad70772986
PC <kim000183@naver.com>
2022-11-03 20:43:18 +0900

    템플릿 기능 리팩토링

--- node/web1_html_internet-master/main.js
+++ node/web1_html_internet-master/main.js
@@ -3,53 +3,56 @@ const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 
-function templateHTML(title, list, body, control){
-  return ` <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-  </body>
-  </html>`;
+const template = {
+  HTML: function (title, list, body, control) {
+    return ` <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>`;
+  },
+  List: function (filelist) {
+    let list = '<ul>';
+    for (let i = 0; i < filelist.length; i++) {
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    }
+    list = list + '</ul>'
+    return list
+  }
 }
 
-function templateList(filelist){
-  let list = '<ul>';
-  for (let i = 0; i < filelist.length; i++) {
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-  }
-  list = list + '</ul>'
-  return list
-} 
-
-const app = http.createServer((request, response) => { 
+const app = http.createServer((request, response) => {
   const _url = request.url;
   const queryData = url.parse(_url, true).query;
   const pathname = url.parse(_url, true).pathname;
-  const title = queryData.id;
+  let title = queryData.id;
   if (pathname === "/") {
     if (title === undefined) {
       fs.readdir('./data', (err, filelist) => {
         title = 'Welcome';
         description = "Hello, Node.js"
-        const list = templateList(filelist);
-        const template = templateHTML(title, list,
+        const list = template.List(filelist);
+        const html = template.HTML(title, list,
           `<h2>${title}</h2>${description}`,
           `<a href="/create">create</a>`)
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
+
+
       });
     } else {
       fs.readdir('./data', (err, filelist) => {
         fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-          const list = templateList(filelist);
-          const template = templateHTML(title, list,
+          const list = template.List(filelist);
+          const html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a> 
             <a href="/update?id=${title}">update</a>
@@ -58,15 +61,15 @@ const app = http.createServer((request, response) => {
               <input type="submit" value="delete">
             </form>`)
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       });
     }
   } else if (pathname === '/create') {
     fs.readdir('./data', (err, filelist) => {
       title = 'WEB - create';
-      const list = templateList(filelist);
-      const template = templateHTML(title, list,`
+      const list = template.List(filelist);
+      const html = template.HTML(title, list, `
       <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -90,27 +93,27 @@ const app = http.createServer((request, response) => {
         </html>
       `, ``)
       response.writeHead(200);
-      response.end(template);
+      response.end(html);
     });
-  } else if(pathname === '/create_process') {
+  } else if (pathname === '/create_process') {
     let body = '';
-      request.on('data', (data) => {
+    request.on('data', (data) => {
       body += data;
     });
-    request.on('end', () =>{
+    request.on('end', () => {
       const post = qs.parse(body)
       const title = post.title;
       const description = post.description;
       fs.writeFile(`data/${title}`, description, `utf-8`, (err) => {
-        response.writeHead(302, {Location: `/?id=${title}`});
+        response.writeHead(302, { Location: `/?id=${title}` });
         response.end('success');
       })
     });
-  } else if(pathname === `/update`) {
+  } else if (pathname === `/update`) {
     fs.readdir('./data', (err, filelist) => {
       fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-        const list = templateList(filelist);
-        const template = templateHTML(title, list,
+        const list = template.List(filelist);
+        const html = template.HTML(title, list,
           `
           <!DOCTYPE html>
           <html lang="en">
@@ -137,36 +140,36 @@ const app = http.createServer((request, response) => {
           `,
           `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`)
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       });
     });
-  } else if(pathname === `/update_process`) {
+  } else if (pathname === `/update_process`) {
     let body = '';
-      request.on('data', (data) => {
+    request.on('data', (data) => {
       body += data;
     });
-    request.on('end', () =>{
+    request.on('end', () => {
       const post = qs.parse(body)
       const id = post.id
       const title = post.title;
       const description = post.description;
-      fs.rename(`data/${id}`,`data/${title}`, (err) => {
+      fs.rename(`data/${id}`, `data/${title}`, (err) => {
         fs.writeFile(`data/${title}`, description, `utf-8`, (err) => {
-          response.writeHead(302, {Location: `/?id=${title}`});
+          response.writeHead(302, { Location: `/?id=${title}` });
           response.end('success');
         })
       });
     });
-  } else if(pathname === `/delete_process`) {
+  } else if (pathname === `/delete_process`) {
     let body = '';
-      request.on('data', (data) => {
+    request.on('data', (data) => {
       body += data;
     });
-    request.on('end', () =>{
+    request.on('end', () => {
       const post = qs.parse(body)
       const id = post.id
-      fs.unlink(`data/${id}`, (err) =>{
-        response.writeHead(302, {Location: `/`});
+      fs.unlink(`data/${id}`, (err) => {
+        response.writeHead(302, { Location: `/` });
         response.end();
       })
     });
