69d9d07cfee92c80b4b2ebda08708c4c6ae830e1
PC <kim000183@naver.com>
2022-11-12 21:10:23 +0900

    코드수정 및 기능구현완료

--- programmers/js/프로그래밍 언어 검색.js
+++ programmers/js/프로그래밍 언어 검색.js
@@ -1,49 +1,63 @@
-const findLanguages = document.querySelector('.SearchInput__input');
+const searchInput = document.querySelector('.SearchInput__input');
 const showListbox = document.querySelector('.Suggestion');
-findLanguages.focus();
-async function showGetdata(e) {
+const showselectLanguage = document.querySelector('.SelectedLanguage');
+const languageArr = JSON.parse(localStorage.getItem('languageList')) ?? [];
+let accounts = [];
+
+searchInput.focus();
+
+async function getdata() {
   try {
-    if (findLanguages.value.length > 0) {
-      const resppnse = await fetch(`https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev/languages?keyword=${findLanguages.value}`, {
-        method: 'GET'
-      });
-      accounts = await resppnse.json();
-      if (e.key !== "ArrowUp" && e.key !== "ArrowDown") {
-        selectLanguages(accounts);
-      }
-      findListchoice(e);
-    }
+    const resppnse = await fetch(`https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev/languages?keyword=${searchInput.value}`, {
+      method: 'GET'
+    });
+    accounts = await resppnse.json();
   } catch (err) {
-    // console.log(err)
+    console.log(err)
   }
 }
 
-async function selectLanguages(list) {
+async function viewList(e) {
+  try {
+    if (searchInput.value.length > 0) {
+      getdata();
+      if (e.key !== "ArrowUp" && e.key !== "ArrowDown" && e.keyCode !== 13) {
+        searchData(accounts);
+      }
+      selectkeyboard(e);
+    } else {
+      showListbox.style.display = "none";
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function searchData(list) {
   showListbox.style.display = "block";
   showListbox.innerHTML = '';
+  let documentFragment = '';
   const ul = document.createElement("ul");
   for (let i = 0; i < list.length; i++) {
-    ul.innerHTML += `
+    documentFragment += `
             <li>
-            ${list[i].replace(findLanguages.value, `<span class="Suggestion__item--matched">${findLanguages.value}</span>`)}
+            ${list[i].replace(searchInput.value, `<span class="Suggestion__item--matched">${searchInput.value}</span>`)}
             </li>
         `
   }
+  ul.innerHTML = documentFragment;
   showListbox.append(ul);
-  const keywordsList = showListbox.querySelectorAll("ul > li");
-  keywordsList[0].classList.add("Suggestion__item--selected");
-  console.log(123)
+  const keywordsList = showListbox.querySelector("ul > li");
+  keywordsList.classList.add("Suggestion__item--selected");
 }
 
-function findListchoice(e) {
+function selectkeyboard(e) {
   const keywordsList = showListbox.querySelectorAll("ul > li");
   const selectedKeyword = showListbox.querySelector("ul > li.Suggestion__item--selected");
-  console.log(selectedKeyword)
   if (e.key === "ArrowUp" || e.key === "ArrowDown") {
     let target;
     const initIndex = e.key === "ArrowUp" ? keywordsList.length - 1 : 0;
     const adjacentSibling = selectedKeyword && (e.key === "ArrowUp" ? selectedKeyword.previousElementSibling : selectedKeyword.nextElementSibling);
-    console.log(adjacentSibling)
     if (adjacentSibling) {
       target = adjacentSibling;
     } else {
@@ -51,8 +65,43 @@ function findListchoice(e) {
     }
     selectedKeyword.classList.remove("Suggestion__item--selected");
     target.classList.add("Suggestion__item--selected");
-    findLanguages.value = target.textContent;
+  } else if (e.key === 'Enter') {
+    alert(selectedKeyword.innerText)
+    choiceList(selectedKeyword.innerText);
   }
 }
-findLanguages.addEventListener('keyup', showGetdata);
-findLanguages.addEventListener('keyup', findListchoice);
+
+function clickSelectLanguage(e) {
+  choiceList(e.target.innerText);
+}
+
+function choiceList(language) {
+  if (languageArr.includes(language)) {
+    const firstCaseIndex = languageArr.indexOf(language);
+    languageArr.splice(firstCaseIndex,1);
+  }
+  languageArr.push(language);
+  showselectLanguage.innerHTML = '';
+  if (languageArr.length > 5) {
+    languageArr.shift();
+  }
+  render();
+  localStorage.setItem("languageList", JSON.stringify(languageArr));
+}
+
+function render() {
+  let documentFragment = '';
+  const ul = document.createElement("ul");
+  for (let i = 0; i < languageArr.length; i++) {
+    documentFragment += `
+            <li>
+            ${languageArr[i]}
+            </li>
+        `
+  }
+  ul.innerHTML = documentFragment;
+  showselectLanguage.append(ul);
+}
+render();
+searchInput.addEventListener('keyup', viewList);
+showListbox.addEventListener('mousedown', clickSelectLanguage);
