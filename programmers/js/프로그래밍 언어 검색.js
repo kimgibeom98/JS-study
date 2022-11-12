924afceb6cdcf920e1d6c339d9b90bb29f2816a9
PC <kim000183@naver.com>
2022-11-12 21:26:23 +0900

    네이밍 수정중

--- programmers/js/프로그래밍 언어 검색.js
+++ programmers/js/프로그래밍 언어 검색.js
@@ -1,12 +1,12 @@
 const searchInput = document.querySelector('.SearchInput__input');
 const showListbox = document.querySelector('.Suggestion');
 const showselectLanguage = document.querySelector('.SelectedLanguage');
-const languageArr = JSON.parse(localStorage.getItem('languageList')) ?? [];
+const languages = JSON.parse(localStorage.getItem('languageList')) ?? [];
 let accounts = [];
 
 searchInput.focus();
 
-async function getData() {
+async function requestLanguages() {
   try {
     const resppnse = await fetch(`https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev/languages?keyword=${searchInput.value}`, {
       method: 'GET'
@@ -20,9 +20,9 @@ async function getData() {
 async function viewList(e) {
   try {
     if (searchInput.value.length > 0) {
-      getData();
+      requestLanguages();
       if (e.key !== "ArrowUp" && e.key !== "ArrowDown" && e.keyCode !== 13) {
-        searchData(accounts);
+        searchData();
       }
       selectkeyboard(e);
     } else {
@@ -33,15 +33,15 @@ async function viewList(e) {
   }
 }
 
-async function searchData(list) {
+async function searchData() {
   showListbox.style.display = "block";
   showListbox.innerHTML = '';
   let documentFragment = '';
   const ul = document.createElement("ul");
-  for (let i = 0; i < list.length; i++) {
+  for (let i = 0; i < accounts.length; i++) {
     documentFragment += `
             <li>
-            ${list[i].replace(searchInput.value, `<span class="Suggestion__item--matched">${searchInput.value}</span>`)}
+            ${accounts[i].replace(searchInput.value, `<span class="Suggestion__item--matched">${searchInput.value}</span>`)}
             </li>
         `
   }
@@ -67,35 +67,35 @@ function selectkeyboard(e) {
     target.classList.add("Suggestion__item--selected");
   } else if (e.key === 'Enter') {
     alert(selectedKeyword.innerText)
-    choiceList(selectedKeyword.innerText);
+    selectLangage(selectedKeyword.innerText);
   }
 }
 
 function clickSelectLanguage(e) {
-  choiceList(e.target.innerText);
+  selectLangage(e.target.innerText);
 }
 
-function choiceList(language) {
-  if (languageArr.includes(language)) {
-    const firstCaseIndex = languageArr.indexOf(language);
-    languageArr.splice(firstCaseIndex,1);
+function selectLangage(language) {
+  if (languages.includes(language)) {
+    const firstCaseIndex = languages.indexOf(language);
+    languages.splice(firstCaseIndex,1);
   }
-  languageArr.push(language);
+  languages.push(language);
   showselectLanguage.innerHTML = '';
-  if (languageArr.length > 5) {
-    languageArr.shift();
+  if (languages.length > 5) {
+    languages.shift();
   }
   render();
-  localStorage.setItem("languageList", JSON.stringify(languageArr));
+  localStorage.setItem("languageList", JSON.stringify(languages));
 }
 
 function render() {
   let documentFragment = '';
   const ul = document.createElement("ul");
-  for (let i = 0; i < languageArr.length; i++) {
+  for (let i = 0; i < languages.length; i++) {
     documentFragment += `
             <li>
-            ${languageArr[i]}
+            ${languages[i]}
             </li>
         `
   }
