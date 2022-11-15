8ad4a00bb41e71db2de0d0a710d0d4c81e06c881
PC <kim000183@naver.com>
2022-11-15 22:00:10 +0900

    동적생성 구현중

--- JS/js/paging.js
+++ JS/js/paging.js
@@ -1,18 +1,39 @@
 const dataArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 const pagingBox = document.querySelector('#paging');
 const showData = document.querySelector('#viewdata');
+const pageCount = 5;
+const pagaeGroup = Math.ceil(dataArr.length / pageCount);
+let createDiv = '';
+let maxNumber;
+let minNumber;
+
+for(let i = 1; i <= pagaeGroup; i++){
+  createDiv += `<button type="button">${i}</button>`
+}
+pagingBox.innerHTML = createDiv;
+const pageNum = document.querySelector('button');
+pageNum.classList.add('active');
 
 function clickEvethandling(e) {
   const pageNum = document.querySelector('button.active');
   pageNum.classList.remove('active');
   e.target.classList.add('active');
-  const currentPage = e.target.innerText;
-  const pageCount = 5;
-  const maxNumber = currentPage * pageCount;
-  const minNumber = maxNumber - pageCount;
+
+  defalutShowdata();
+}
+
+function defalutShowdata(){
+  const pageNum = document.querySelector('button.active');
+  const currentPage = pageNum.innerText;
+
+  maxNumber = currentPage * pageCount;
+  minNumber = maxNumber - pageCount;
 
   const resultData = dataArr.slice(minNumber, maxNumber);
   showData.innerHTML = '';
   showData.innerHTML += resultData.map((el) => `<span>${el}</span>`)
 }
+
+defalutShowdata();
 pagingBox.addEventListener('click', clickEvethandling) 
+
