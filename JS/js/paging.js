442d52cef734c87ea9d7b62e1278ad2786efd805
PC <kim000183@naver.com>
2022-11-15 21:30:18 +0900

    데이터 뿌려주기

--- JS/js/paging.js
+++ JS/js/paging.js
@@ -1,5 +1,6 @@
 const dataArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 const pagingBox = document.querySelector('#paging');
+const showData = document.querySelector('#viewdata');
 
 function clickEvethandling(e) {
   const pageNum = document.querySelector('button.active');
@@ -10,7 +11,8 @@ function clickEvethandling(e) {
   const maxNumber = currentPage * pageCount;
   const minNumber = maxNumber - pageCount;
 
-
-  console.log(dataArr.slice(minNumber, maxNumber));
+  const resultData = dataArr.slice(minNumber, maxNumber);
+  showData.innerHTML = '';
+  showData.innerHTML += resultData.map((el) => `<span>${el}</span>`)
 }
 pagingBox.addEventListener('click', clickEvethandling) 
