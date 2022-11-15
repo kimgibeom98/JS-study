80093dc0540728ca73e1cabb2f3d8cc362c5625d
PC <kim000183@naver.com>
2022-11-15 21:14:18 +0900

    변수 제거

--- JS/js/paging.js
+++ JS/js/paging.js
@@ -1,6 +1,5 @@
 const dataArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 const pagingBox = document.querySelector('#paging');
-const showData = document.querySelector('#viewdata');
 
 function clickEvethandling(e) {
   const pageNum = document.querySelector('button.active');
@@ -10,6 +9,7 @@ function clickEvethandling(e) {
   var pageCount = 5;
   var maxNumber = currentPage * pageCount;
   var minNumber = maxNumber - pageCount;
+
 
   console.log(dataArr.slice(minNumber, maxNumber));
 }
