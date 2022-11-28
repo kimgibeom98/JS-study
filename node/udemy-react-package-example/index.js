f7cb49a6ca0a640c68597fc492e4d74a3f37be96
PC <kim000183@naver.com>
2022-11-28 22:51:37 +0900

    npm 외부모듈 사용

--- node/udemy-react-package-example/index.js
+++ node/udemy-react-package-example/index.js
@@ -1,1 +1,8 @@
-console.log('hello nodejs pacackge')
+const randomcolor = require('randomcolor');
+
+const color1 = randomcolor();
+const color2 = randomcolor();
+const color3 = randomcolor();
+
+console.log(color1, color2, color3)
+
