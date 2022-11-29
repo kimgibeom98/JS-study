1543e2d10871d169536279cc00f1028e8394ba2f
PC <kim000183@naver.com>
2022-11-29 22:43:02 +0900

    React스터디

---
+++ reactexam1/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "김기범";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 리액트 {name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
