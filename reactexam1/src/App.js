5d072285a4b994323a4cff48d425873e345f7e6e
PC <kim000183@naver.com>
2022-11-30 21:22:04 +0900

    조건부 랜더링

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -19,11 +19,15 @@ function App() {
     }
   };
 
+  const number = 5;
+
   return (
     <div style={style.App}>
       <MyHeader />
-      <h2 style={style.h2}>안녕 리액트 {name}</h2>
-      <b style={style.bold_text}> id='bold_text'>React.js</b>
+      <h2 style={style.h2}>안녕 리액트 {false}</h2>
+      <b style={style.bold_text} id='bold_text'>
+        {number} 는 {number % 2 === 0 ? "짝수" : "홀수"}
+      </b>
       <MyFooter />
     </div >
   );
