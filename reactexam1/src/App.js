54ea6f711e682c136b142ddbe67ababc169f9864
PC <kim000183@naver.com>
2022-11-30 21:57:20 +0900

    state

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,35 +1,19 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+
+import Counter from './Counter';
 import MyHeader from './MyHeader'
-import MyFooter from './MyFooter'
+
+// import MyFooter from './MyFooter'
 
 function App() {
-  const name = "김기범";
-
-  const style = {
-    App: {
-      backgroundColor: 'black'
-    },
-    h2: {
-      color: 'red'
-    },
-    bold_text: {
-      color: 'green'
-    }
-  };
-
-  const number = 5;
 
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader />
-      <h2 style={style.h2}>안녕 리액트 {false}</h2>
-      <b style={style.bold_text} id='bold_text'>
-        {number} 는 {number % 2 === 0 ? "짝수" : "홀수"}
-      </b>
-      <MyFooter />
-    </div >
+      <Counter />
+    </div>
   );
 }
 
