8bcf41871d281a17e376850042f6a5f1063b79db
PC <kim000183@naver.com>
2022-11-30 22:35:30 +0900

    props

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -3,17 +3,29 @@
 
 
 import Counter from './Counter';
+import Container from './Container';
 import MyHeader from './MyHeader'
 
 // import MyFooter from './MyFooter'
 
 function App() {
 
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    initialValue: 5
+  }
+
   return (
-    <div>
-      <MyHeader />
-      <Counter />
-    </div>
+    <Container>
+      <div>
+        <MyHeader />
+        <Counter {...counterProps} />
+      </div>
+    </Container>
   );
 }
 
--- reactexam1/src/Counter.js
+++ reactexam1/src/Counter.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
+import OddEvenResult from './OddEvenResult';
 
-const Counter = () => {
-    const [count, setCount] = useState(0);
-
+const Counter = ({ initialValue }) => {
+    const [count, setCount] = useState(initialValue);
     const onIncrease = () => {
         setCount(count + 1);
     }
@@ -16,8 +16,15 @@ const Counter = () => {
             <h2>{count}</h2>
             <button onClick={onIncrease}>+</button>
             <button onClick={onDecrease}>-</button>
+            <OddEvenResult count={count} />
         </div>
     )
+}
+
+// 넘겨준 props가 없는 객체일때는 0을 기본값으로 한다
+
+Counter.defaultProps = {
+    initialValue: 0,
 }
 
 export default Counter;
