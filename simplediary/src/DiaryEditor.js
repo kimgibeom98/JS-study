a255ebcd6fce83fb59117fe6e1796c2fd98f8de4
PC <kim000183@naver.com>
2022-12-01 22:47:53 +0900

    일기장 form완성

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -13,7 +13,10 @@ const DiaryEditor = () => {
             [e.target.name]: e.target.value,
         })
     }
-
+    const handelSubmit = () => {
+        console.log(state)
+        alert("저장성공")
+    }
     return <div className="DiaryEditor">
         <h2>오늘의 일기</h2>
         <div>
@@ -23,6 +26,7 @@ const DiaryEditor = () => {
             <textarea name="content" value={state.content} onChange={handleChangeState} />
         </div>
         <div>
+            <span>오늘의 감정점수 : </span>
             <select name="emotion" value={state.emotion} onChange={handleChangeState}>
                 <option value={1}>1</option>
                 <option value={2}>2</option>
@@ -30,6 +34,9 @@ const DiaryEditor = () => {
                 <option value={4}>4</option>
                 <option value={5}>5</option>
             </select>
+        </div>
+        <div>
+            <button onClick={handelSubmit}>일기 저장하기</button>
         </div>
     </div >
 }
