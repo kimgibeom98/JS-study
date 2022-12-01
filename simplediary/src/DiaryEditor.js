3ed7a74650b95543b334dd4bdc689323108aeb3e
PC <kim000183@naver.com>
2022-12-01 22:39:37 +0900

    textarea, select 추가

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,14 +1,36 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
-    const [author, setAuthor] = useState("김기범");
+    const [state, setState] = useState({
+        author: '',
+        content: '',
+        emotion: 1
+    })
 
+    const handleChangeState = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        })
+    }
 
     return <div className="DiaryEditor">
         <h2>오늘의 일기</h2>
         <div>
-            <input value={author} />
+            <input name="author" value={state.author} onChange={handleChangeState} />
         </div>
-    </div>
+        <div>
+            <textarea name="content" value={state.content} onChange={handleChangeState} />
+        </div>
+        <div>
+            <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+            </select>
+        </div>
+    </div >
 }
 export default DiaryEditor;
