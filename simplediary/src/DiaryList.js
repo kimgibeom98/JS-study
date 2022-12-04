690e1bf6c580c9655c686e17517d72bb09d1b4c9
PC <kim000183@naver.com>
2022-12-04 20:21:17 +0900

    렌더링 완료

--- simplediary/src/DiaryList.js
+++ simplediary/src/DiaryList.js
@@ -1,3 +1,5 @@
+import DiaryItem from "./DiaryItem";
+
 const DiaryList = ({ diaryList }) => {
     console.log(diaryList)
     return <div className="DiaryList">
@@ -5,12 +7,8 @@ const DiaryList = ({ diaryList }) => {
         <h4>{diaryList.length}개의 일기가 있습니다.</h4>
         <div>
             {diaryList.map((it) => (
-                <div>
-                    <div>작성자 : {it.author}</div>
-                    <div>일기 : {it.content}</div>
-                    <div>감정 : {it.emotion}</div>
-                    <div>작성시간(ms) : {it.created_date}</div>
-                </div>
+                <DiaryItem key={it.id} {...it} />
+
             ))}
         </div>
     </div>
