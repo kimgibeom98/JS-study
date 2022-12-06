7135611a3bf9e55a22cc2729b5ab2eedbf944755
PC <kim000183@naver.com>
2022-12-06 21:18:00 +0900

    API불러와서 fetch와 사용해보기

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,9 +1,9 @@
 // import logo from './logo.svg';
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import Lifecycle from "./Lifecycle";
+// import Lifecycle from "./Lifecycle";
 
 
 
@@ -11,13 +11,38 @@ function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime() + 1,
+        id: dataId.current++
+      };
+    })
+
+    setData(initData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
   const onCreate = (author, content, emotion) => {
-    const created_data = new Date().getTime();
+    const created_date = new Date().getTime();
     const newItem = {
       author,
       content,
       emotion,
-      created_data,
+      created_date,
       id: dataId.current
     }
     dataId.current += 1;
@@ -37,7 +62,7 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
+      {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
