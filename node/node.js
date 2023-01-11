61f87bad60fd31cecee0023d5e718e0c11a328a5
PC <kim000183@naver.com>
2023-01-11 11:33:14 +0900

    파일 수정 및 npm 이론정리

--- node/node.js
+++ node/node.js
@@ -1,1 +1,5 @@
-console.log(1+1)
+// npm install = npm i
+// npm install vue --save-dev = npm i vue -d
+
+// dependencies : npm i jquery 로 설치했을때 dependencies - 화면 dom조작을 도와주는 라이브러리 - application을 동작시킬때 필요한 라이브러리 ex) vue, react, angular, chart 등
+// devDependencies : npm i webpack - d 로 설치했을때 devDependencies - webpack, sass 등 개발을 할때 도움을 주는 개발보조 라이브러리, npm run build 했을때 포함 안됨 그렇기때문에 화면조작에 필요한 라이브 러리라고 생각되면 dependencies에 넣어줘야함
