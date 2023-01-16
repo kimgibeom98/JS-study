7a963cdfa6b1004bae9e0a18b970298e0a0d5027
PC <kim000183@naver.com>
2023-01-16 21:45:03 +0900

    interface 확장 및 extends

--- TS/class-note/3_interface.ts
+++ TS/class-note/3_interface.ts
@@ -19,3 +19,50 @@ const capt = {
     age : 100
 }
 getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+
+interface SumFunction {
+    (a : number, b : number) : number
+}
+
+let sum : SumFunction;
+sum = function(a : number, b : number) : number {
+    return a + b;
+}
+
+// 인덱싱
+interface stringArray {
+     [index : number] : string
+}
+
+let arr02 : stringArray = ['a', 'b', 'c'];
+arr02[0] = '10'
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+    [key : string] : RegExp;
+}
+
+let obj02 : StringRegexDictionary = {
+    cssFile : /\.css$/ 
+}
+
+Object.keys(obj02).forEach(function(value) {});
+
+// 인터페이스 확장
+
+interface person {
+    name : string,
+    age : number
+};
+
+interface Developer extends person {
+    Language : string
+}
+
+let captain : Developer = {
+    Language : 'ts',
+    name : '캡틴',
+    age : 100
+}
