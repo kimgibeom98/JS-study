7dbb4af47b5d44b0dc82764babffaadcdc703821
PC <kim000183@naver.com>
2023-02-08 22:26:44 +0900

    제네릭 study

--- TS/class-note/8_generics.ts
+++ TS/class-note/8_generics.ts
@@ -27,9 +27,9 @@
 
 // 위와 같은 문제들을 제네릭으로 아래와 같이 해결
 
-function logText<T>(text : T ) : T {
-    console.log(text);
-    return text;
+function logText<T>(text: T): T {
+  console.log(text);
+  return text;
 }
 const str = logText<string>('abc');
 str.split('');
@@ -37,3 +37,57 @@ str.split('');
 const login = logText<boolean>(true);
 
 // 제네릭 - 타입을 비워놓은 상태에서 타입에 뭐가 들어가는지 호출하는 시점에서 정의함.
+
+//인터페이스에 제네릭을 선언하는 방법
+
+// interface Dropdown{
+//     value : string;
+//     selected : boolean;
+// }
+// const obj : Dropdown = {value : 'abc', selected : false}
+
+// 위를 아래처럼
+
+interface Dropdonw<T> {
+  value: T;
+  selected: boolean
+}
+const obj: Dropdonw<string> = { value: 'abc', selected: false }
+
+// 제네릭 타입 제한
+// function logTextLength<T>(text: T[]): T[] {
+//   console.log(text.length);
+//   text.forEach(function (text) {
+//     console.log(text);
+//   });
+//   return text;
+// }
+// logTextLength<string>(['hi', 'abc']);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+
+function logTextLength<T extends LengthType>(text : T) : T {
+  text.length;
+  return text;
+}
+logTextLength(10);
+logTextLength({length : 10});
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem {
+  name : string;
+  price : number;
+  stock : number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>( itemOption : T) : T{
+  return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption('a');
+
+getShoppingItemOption("name");
