f037c70cd85b0cf8fdae71ef2ae88b2bd4ac0cc8
PC <kim000183@naver.com>
2023-02-15 22:21:27 +0900

    다시수정 - 타입호환

--- TS/class-note/12_type-compatibility.ts
+++ TS/class-note/12_type-compatibility.ts
@@ -1,45 +1,45 @@
-// 인터페이스
+// interface
 interface Developer {
   name: string;
   skill: string;
 }
-// interface Person {
-//   name: string;
-//   // skill: string;
-// }
-class Person {
+
+interface Person {
   name: string;
-  skill: string;
 }
 
-var developer: Developer;
-var person: Person;
-developer = new Person();
-// person = developer;
+let developer: Developer;
+let person: Person;
+// developer = person; // X
+// person = developer; // o
+
+// https://joshua1988.github.io/ts/guide/type-compatibility.html 이론 읽기!
+
 
 // 함수
-var add = function(a: number) {
-  // ...
+let add = function (a: number) {
+  //...
 }
-var sum = function(a: number, b: number) {
-  // ...
+
+let sum = function (a: number, b: number) {
+  //...
 }
-sum = add;
-// add = sum;
+// sum = add; // O
+// add = sum; // x
 
 // 제네릭
 interface Empty<T> {
   // ..
 }
-var empty1: Empty<string>;
-var empty2: Empty<number>;
-empty1 = empty2;
-empty2 = empty1;
+let empty1: Empty<string>;
+let empty2: Empty<number>;
+// empty1 = empty2; 0
+// empty2 = empty1; o
 
 interface NotEmpty<T> {
   data: T;
 }
-var notempty1: NotEmpty<string>;
-var notempty2: NotEmpty<number>;
-notempty1 = notempty2;
-notempty2 = notempty1;
+let notempty1: NotEmpty<string>;
+let notempty2: NotEmpty<number>;
+// notempty1 = notempty2; x
+// notempty2 = notempty1; x
