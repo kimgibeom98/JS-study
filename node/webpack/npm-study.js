1a4e134304999a0c77cbcec189359dbaa494f865
PC <kim000183@naver.com>
2023-03-06 20:17:36 +0900

    npm study webpack시작

---
+++ node/webpack/npm-study.js
@@ -0,0 +1,11 @@
+// npm install = npm i
+// npm install "라이브러리 네임" - 라이브러리 추가할때
+// npm uninstall "라이브러리 네임" - 라이브러리 삭제할때
+// npm install "라이버르러 네임" --global - 전역으로 설치됨 -> --global = -g 로 축약가능
+
+// npm install vue --save-dev = npm i vue -d
+
+// dependencies - 배포용 : npm i jquery 로 설치했을때 dependencies - 화면 dom조작을 도와주는 라이브러리 - application을 동작시킬때 필요한 라이브러리 ex) vue, react, angular, chart, jquery 등
+// devDependencies - 개발용 : npm i webpack - d 로 설치했을때 devDependencies - webpack, sass 등 개발을 할때 도움을 주는 개발보조 라이브러리,
+// npm run build 했을때 포함 안됨 그렇기때문에 화면조작에 필요한 라이브 러리라고 생각되면 dependencies에 넣어줘야함 ex) webpack, js-compression, sass 등
+
