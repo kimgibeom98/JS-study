a878314e36780e2f5860ff8096d9111c528a71be
PC <kim000183@naver.com>
2023-04-18 23:52:29 +0900

    설정파일 분석

--- node/webpack/code-splitting/webpack.config.js
+++ node/webpack/code-splitting/webpack.config.js
@@ -1,18 +1,22 @@
 var path = require('path');
 
 module.exports = {
-  mode: 'none',
-  entry: './index.js',
+  mode: 'none', // production, development, none -> mode에는 3가지가 있지만 실제로 배포할때는 production을 사용
+  entry: './index.js',  // 웹팩으로 변환할 대상 파일의 경로
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
+  }, // dist라는 폴더의 경로를 만들어서 bundel.js를 생성
+  module: { // Loader
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      }
+      },// babel을 사용할 경우 아래와 같이
+      // { 
+      //   test : /\.js$/,
+      //   use : ['babel-loader']
+      // }
     ]
   },
 }
