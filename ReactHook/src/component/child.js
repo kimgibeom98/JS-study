6e2000ef77925f6cf899bc83d9459e2bad1cac61
PC <kim000183@naver.com>
2023-06-01 21:25:50 +0900

    useMemo랑 ReactMemo같이 쓰기

--- ReactHook/src/component/child.js
+++ ReactHook/src/component/child.js
@@ -1,12 +1,12 @@
 import { memo } from 'react';
 
-const Child = ({ name, age }) => {
+const Child = ({ name }) => {
   console.log('자녀 컴포넌트 렌더링')
   return (
     <div style={{ border: '2px solid powderblue', padding: '10px' }}>
       <h3>자녀</h3>
-      <p>name : {name}</p>
-      <p>age : {age}</p>
+      <p>성 : {name.lastName}</p>
+      <p>이름 : {name.firstName}</p>
     </div>
   )
 }
--- ReactHook/src/hookfile/reactmemo.js
+++ ReactHook/src/hookfile/reactmemo.js
@@ -1,29 +1,29 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import Child from '../component/child'
 
 const ReactMemo = () => {
 
   const [parentAge, setParentAge] = useState(0);
-  const [childAge, setChildAge] = useState(0)
 
   const incrementParentAge = () => {
     setParentAge(parentAge + 1);
   }
 
-  const incrementChildAge = () => {
-    setChildAge(childAge + 1);
-  }
-
   console.log('부모 컴포넌트 렌더링')
+  const name = useMemo(() => {
+    return {
+      lastName: '홍',
+      firstName: '길동'
+    }
+  }, []);
 
   return (
     <div style={{ border: '2px solid navy', padding: '10px' }}>
       <h1>부모</h1>
       <p>age : {parentAge}</p>
       <button onClick={incrementParentAge}>부모 나이 증가</button>
-      <button onClick={incrementChildAge}>자녀 나이 증가</button>
-      <Child name={'홍기동'} age={childAge} />
+      <Child name={name} />
     </div>
   )
 
